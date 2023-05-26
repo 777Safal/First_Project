1a223e10850ff0e9202e91899b8e8aab8a7b3b0b
777Safal <tsafal06@gmail.com>
2023-05-26 17:13:55 +0545

    first step done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Toolbar from './Components/Navigation/Toolbar';
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      <Toolbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navigation/Toolbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Toolbar() {
+  return (
+    <div className='w-full flex justify-center'>
+        <div className='w-9/12 text-red-600 font-bold text-3xl bg-red-500 h-[95]'>
+          
+        </div>
+    </div>
+  )
+}
+
+export default Toolbar
