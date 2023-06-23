2a6a51bd3c6d7a5a7a78dbf946c0c8579835d85f
777Safal <tsafal06@gmail.com>
2023-06-23 08:39:32 +0545

    new code

--- src/Components/Page Component/Elements/Switch.jsx
+++ src/Components/Page Component/Elements/Switch.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+
 
 function Switch() {
+    const [toggle,settoggle]=useState(true)
+    const afterClick=()=>{
+        settoggle(!toggle)
+    }
   return (
     <div>
-        <label className='relative inline-block w-16 h-9'>
-            <input className='opacity-0 '/>
-            <span className='absolute cursor-pointer top-0 bottom-0 left-0 right-0 bg-gray-300 ease-linear duration-150'/>
-        </label>
+      <div onClick={afterClick} className={`${toggle? 'bg-primary':'bg-hov'} p-1 w-24 h-10 ml-5 mt-5 shadow-inner rounded-3xl`}> 
+      <div className='bg-white rounded-3xl relative w-full h-full outline-none'>
+      <button  className='w-full h-full bg-transparent outline-none'></button>
+      <div onClick={afterClick} className={`${toggle ? 'bg-primary absolute left-0 top-0':'bg-hov absolute right-0 top-0'} w-2/4 h-8 absolute transform transition-transform ease-in-out duration-300 top-0 left-0 right-0 bottom-0 shadow-inner rounded-3xl outline-none`}></div>
+      </div>
+      </div>
     </div>
   )
 }
