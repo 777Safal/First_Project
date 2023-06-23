d5aca43a8b693fa2eaaee30fb1343d05924bdb66
777Safal <tsafal06@gmail.com>
2023-06-23 08:54:08 +0545

    switch done

--- src/Components/Page Component/Elements/Switch.jsx
+++ src/Components/Page Component/Elements/Switch.jsx
@@ -11,7 +11,7 @@ function Switch() {
       <div onClick={afterClick} className={`${toggle? 'bg-primary':'bg-hov'} p-1 w-24 h-10 ml-5 mt-5 shadow-inner rounded-3xl`}> 
       <div className='bg-white rounded-3xl relative w-full h-full outline-none'>
       <button  className='w-full h-full bg-transparent outline-none'></button>
-      <div onClick={afterClick} className={`${toggle ? 'bg-primary absolute left-0 top-0':'bg-hov absolute right-0 top-0'} w-2/4 h-8 absolute transform transition-transform ease-in-out duration-300 top-0 left-0 right-0 bottom-0 shadow-inner rounded-3xl outline-none`}></div>
+      <div onClick={afterClick} className={`${toggle ? 'bg-primary absolute':'bg-hov absolute transform translate-x-12'} w-2/4 h-8 absolute top-0 left-0 right-0 bottom-0 shadow-inner rounded-3xl outline-none`}></div>
       </div>
       </div>
     </div>
