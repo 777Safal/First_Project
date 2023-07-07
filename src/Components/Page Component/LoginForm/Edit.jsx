53e005c53d29763c5a067234394738cfbf77be59
777Safal <tsafal06@gmail.com>
2023-07-07 17:29:08 +0545

    new code

--- src/Components/Page Component/LoginForm/Edit.jsx
+++ src/Components/Page Component/LoginForm/Edit.jsx
@@ -4,20 +4,15 @@ import EditForm from './EditForm';
 
 
 function Edit({editRef}) {
-  const [editForm,setEditForm]=useState(false)
-  return (
     
+  return ( 
     <dialog ref={editRef}>
-      {
-      editForm==='false'?<EditForm />:''
-    }
       <div className='w-fit h-fit m-auto'><FaRegEdit className='text-7xl text-blue-400'/></div>
     <div className='w-fit mx-auto'>
       Are you sure you want to <span className='text-blue-400 font-semibold'>edit</span>?</div>
     <div className='flex justify-center gird grid-cols-2 gap-4'>
-        <div><button className='px-3 py-1 rounded-md hover:bg-white ' onClick={()=>setEditForm('false')}>Yes</button></div>
+        <div><button className='px-3 py-1 rounded-md hover:bg-white '>Yes</button></div>
         <div><button className='px-3 py-1 rounded-md hover:bg-white' onClick={()=>editRef.current.close()}>No</button></div>
-        
     </div>
 </dialog>
   )
