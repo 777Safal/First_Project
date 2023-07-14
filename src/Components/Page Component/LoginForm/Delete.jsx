ec7fbe0c5b53514926b6eb3697330f11b2b954c3
777Safal <tsafal06@gmail.com>
2023-07-14 09:01:17 +0545

    new code

--- src/Components/Page Component/LoginForm/Delete.jsx
+++ src/Components/Page Component/LoginForm/Delete.jsx
@@ -1,10 +1,13 @@
+import axios from 'axios'
 import Reac,{useRef,useState} from 'react'
 import {RiDeleteBin5Line} from 'react-icons/ri'
 
 function Delete({deleteRef}) {
   const [deleted,setDeleted]=useState(false)
-  const confirmDelete =()={
-
+  const confirmDelete=()=>{
+  setDeleted(
+    axios.delete('http://192.168.1.147:4003/studentinfo/',formData).then(res=>{console.log(res)}).catch(err=>{console.log(err)})
+  )
   }
   return (
         <dialog ref={deleteRef}>
@@ -12,7 +15,7 @@ function Delete({deleteRef}) {
             <div className='w-fit mx-auto'>
               Are you sure you want to <span className='text-red-600 font-semibold'>delete</span>?</div>
             <div className='flex justify-center gird grid-cols-2 gap-4'>
-                <div><button className='px-3 py-1 rounded-md hover:bg-white '>Yes</button></div>
+                <div><button className='px-3 py-1 rounded-md hover:bg-white ' onClick={confirmDelete}>Yes</button></div>
                 <div><button className='px-3 py-1 rounded-md hover:bg-white ' onClick={()=>deleteRef.current.close()}>No</button></div>
             </div>
         </dialog>
